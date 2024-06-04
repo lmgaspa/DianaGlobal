8160b5afec108de0c4a821cb16efebcc84fc403a
Luiz <seu-email@example.com>
2024-06-04 14:03:51 -0300

    final

--- pages/login.tsx
+++ pages/login.tsx
@@ -84,7 +84,7 @@ const Login: React.FC = () => {
               </button>
               <p className="text-center text-sm mt-4 text-black dark:text-white">
                 Don&apos;t have an account?
-                <Link href="/SignUp">
+                <Link href="/signup">
                   <span className="text-blue-500 hover:underline cursor-pointer ml-1">Register here</span>
                 </Link>
               </p>
