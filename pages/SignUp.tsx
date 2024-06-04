02e9b9f1d3835f8bfb42e16a551abf4571a17887
Luiz <seu-email@example.com>
2024-06-04 12:23:00 -0300

    dxg

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -98,7 +98,7 @@ const SignUp: React.FC = () => {
                 className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
                 disabled={isSubmitting}
               >
-                Sign Up
+                Sign Up!
               </button>
             </Form>
           )}
