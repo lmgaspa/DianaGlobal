8107ffc0d6e7b4b488f6ae5c361c340f58c24a7c
Luiz <seu-email@example.com>
2024-06-15 00:43:51 -0300

    vercel

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gXwIHL7PleR834RyuJDfHV1l+5DSCoqr1OOcUryjg4g=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cz1bocNrPEcr2e92MhKNOiEWovw/qQaINpyQDfvuhXg=\"\n}"
