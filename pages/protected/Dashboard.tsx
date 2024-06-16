601137412fc4c46018fa68fbce1d882745fa4f1f
Luiz <seu-email@example.com>
2024-06-16 14:09:59 -0300

    okxsk

--- pages/protected/Dashboard.tsx
+++ pages/protected/Dashboard.tsx
@@ -1,13 +1,9 @@
-// Importe o tipo de contexto do Next.js
-"use client"
-import React from 'react'
-import { GetServerSidePropsContext } from 'next';
-import { getSession } from 'next-auth/react';
+'use client';
+import '../../app/globals.css';
+import React from "react";
 import EstimatedBalance from '@/components/DashBoardComponent/EstimatedBalance';
 import DashLoginComponent from '@/components/DashBoardComponent/DashLoginComponent';
 
-
-// Defina o tipo para Dashboard
 const Dashboard: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-black">
@@ -22,21 +18,5 @@ const Dashboard: React.FC = () => {
     </div>
   );
 };
-
-// Adicione o tipo explícito para context
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: {},
-  };
-}
 
 export default Dashboard;
