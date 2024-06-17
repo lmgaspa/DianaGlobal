130ee0e92e9ee4e3bf72c2c111c9a71a7b8205a2
Luiz <seu-email@example.com>
2024-06-16 21:41:56 -0300

    tesxt

--- pages/protected/Dashboard.tsx
+++ pages/protected/Dashboard.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+'use client';
+import '../../app/globals.css';
+import React from "react";
+import EstimatedBalance from '@/components/DashBoardComponent/EstimatedBalance';
+import DashLoginComponent from '@/components/DashBoardComponent/DashLoginComponent';
 import { GetServerSideProps } from 'next';
 
 interface DashboardProps {
@@ -8,10 +12,15 @@ interface DashboardProps {
 
 const Dashboard: React.FC<DashboardProps> = ({ userId, email }) => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>User ID: {userId}</p>
-      <p>Email: {email}</p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-black">
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
+        <div className="bg-white p-8 rounded shadow-md w-96 h-96 text-center">
+          <DashLoginComponent />
+        </div>
+        <div className="bg-blue-200 p-8 rounded shadow-md w-96 h-96 text-center">
+          <EstimatedBalance />
+        </div>
+      </div>
     </div>
   );
 };
