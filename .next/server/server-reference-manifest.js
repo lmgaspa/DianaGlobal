8ed1ceb2cf408f986afbc797eded1c56cfce4af7
Luiz <seu-email@example.com>
2024-06-23 07:19:05 -0300

    dashboard

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"epgn6DUWK7MUkfFuYqYnbZVuue+o9ChorBCtU0OJ9Kk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M+zTQl/BEuyTYo0Ko4tk3KY5pWcfZ0PbfjkOYeq6R2w=\"\n}"
