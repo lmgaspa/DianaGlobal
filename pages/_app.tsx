4480c7766147162bc928468f1cd20ce4c6602f10
Luiz <seu-email@example.com>
2024-06-24 16:20:26 -0300

    final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,32 @@
 import { ThemeProvider } from '../context/ThemeContext';
 import MainContainer from '../components/GlobalComponent/MainComponent';
 import { AppProps } from 'next/app';
-import React from 'react';
-import { SessionProvider } from "next-auth/react"
-import '../styles/globals.css'
+import React, { useEffect } from 'react';
+import { SessionProvider } from "next-auth/react";
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
-  
+  // Função para limpar o localStorage
+  const clearLocalStorage = () => {
+    localStorage.removeItem('userId');
+    localStorage.removeItem('email');
+  };
+
+  // Efeito para limpar o localStorage ao montar o componente
+  useEffect(() => {
+    clearLocalStorage();
+  }, []);
+
   return (
     <SessionProvider session={session}>
-    <ThemeProvider>
-      <MainContainer>
-        <Component {...pageProps} />
-      </MainContainer>
-    </ThemeProvider>
+      <ThemeProvider>
+        <MainContainer>
+          <Component {...pageProps} />
+        </MainContainer>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
 
 export default MyApp;
+
