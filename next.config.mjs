4b3a45bb59d3ef13bb5d1bc4b78dc9645bc1410d
Luiz <seu-email@example.com>
2024-06-23 23:45:37 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/protected/:path*', // Qualquer rota que comece com /protected/
-                destination: '/protected/dashboard', // Será redirecionada para /protected/dashboard
-            },
-        ];
-    },
 };
 
 export default nextConfig;
