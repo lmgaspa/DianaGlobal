a45294ef5dc64e60dd1819a34fdde958f32e34b5
Luiz <seu-email@example.com>
2024-06-27 22:53:39 -0300

    sol

--- components/DashBoardComponent/EstimatedBalance.tsx
+++ components/DashBoardComponent/EstimatedBalance.tsx
@@ -58,7 +58,7 @@ const EstimatedBalance: React.FC<EstimatedBalanceProps> = ({ userId }) => {
     const fetchSolAddress = async (userId: string) => {
       try {
         console.log('Fetching Solana address for userId:', userId);
-        const response = await axios.post('https://solana-wallet-generator.onrender.com/api/sol_sucess', {
+        const response = await axios.post('https://solana-wallet-generator.onrender.com/api/sol_success', {
           userId: userId,
         });
         const { solAddress } = response.data;
