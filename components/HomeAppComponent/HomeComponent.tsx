e9070bb871be064451cbeb8d58e95c80811c5185
Luiz <seu-email@example.com>
2024-06-30 15:33:41 -0300

    fulltrack

--- components/HomeAppComponent/HomeComponent.tsx
+++ components/HomeAppComponent/HomeComponent.tsx
@@ -6,8 +6,8 @@ const HomeComponent: React.FC = () => {
     <div>
       <main className="flex flex-col md:flex-row items-center justify-between pt-8 md:pt-24 p-11 text-black dark:bg-black dark:text-white">
         {/* Esquerda */}
-        <div className="md:w-1/2 p-8 text-container ml-12">
-          <h1 className="text-7xl font-bold mb-4 ">Start Trading Today</h1>
+        <div className="md:w-1/2 text-container md:ml-12 md:pl-12">
+          <h1 className="text-7xl text-center font-bold mb-4">Start Trading Today</h1>
           <p className="text-lg text-justify mt-6 ">
             Invest in the future with cryptocurrency. Start trading today and unlock the potential of digital assets. Join Diana Global and embark on your journey to financial freedom with cutting-edge technology and expert guidance.
           </p>
