4614b67faebd95881b34cad5da6485abf630622c
Luiz <seu-email@example.com>
2024-07-01 14:34:07 -0300

    tryfixreloadERROR

--- pages/protected/Dashboard.tsx
+++ pages/protected/Dashboard.tsx
@@ -1,38 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import EstimatedBalance from '@/components/DashBoardComponent/EstimatedBalance';
 import DashLoginComponent from '@/components/DashBoardComponent/DashLoginComponent';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 
 interface DashboardProps {
-  initialUserId: string;
-  initialEmail: string;
+  userId: string;
+  email: string;
 }
 
-const Dashboard: React.FC<DashboardProps> = ({ initialUserId, initialEmail }) => {
-  const [userId, setUserId] = useState(initialUserId);
-  const [email, setEmail] = useState(initialEmail);
-
-  useEffect(() => {
-    // Verificar se os dados estão no localStorage
-    const storedUserId = localStorage.getItem('userId');
-    const storedEmail = localStorage.getItem('email');
-    console.log('storedUserId:', storedUserId);
-    console.log('storedEmail:', storedEmail);
-
-    if (storedUserId && storedEmail) {
-      setUserId(storedUserId);
-      setEmail(storedEmail);
-    } else {
-      // Armazenar os dados no localStorage
-      console.log('Setting localStorage userId and email');
-      localStorage.setItem('userId', userId);
-      localStorage.setItem('email', email);
-    }
-  }, [userId, email]);
-
-  console.log('Rendering Dashboard with userId:', userId, 'and email:', email);
-
+const Dashboard: React.FC<DashboardProps> = ({ userId, email }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-black">
       <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
@@ -49,7 +26,6 @@ const Dashboard: React.FC<DashboardProps> = ({ initialUserId, initialEmail }) =>
 
 export const getServerSideProps: GetServerSideProps<DashboardProps> = async (context) => {
   const session = await getSession(context);
-  console.log('Session:', session);
 
   if (!session) {
     return {
@@ -63,7 +39,7 @@ export const getServerSideProps: GetServerSideProps<DashboardProps> = async (con
   const { id: userId, email } = session.user;
 
   if (!userId || !email) {
-    console.error('UserId ou email estão ausentes em session.user');
+    console.error('UserId or email is missing in session.user');
     return {
       redirect: {
         destination: '/login',
@@ -74,8 +50,8 @@ export const getServerSideProps: GetServerSideProps<DashboardProps> = async (con
 
   return {
     props: {
-      initialUserId: userId,
-      initialEmail: email,
+      userId,
+      email,
     },
   };
 };
