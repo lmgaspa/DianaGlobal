098f3dafb935ad0bf44fe229866fd0e3d6d2112a
Luiz <seu-email@example.com>
2024-07-01 15:22:54 -0300

    newsolves

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,13 @@ import { SessionProvider } from "next-auth/react";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
-  // Função para limpar o localStorage
-  const clearLocalStorage = () => {
-    localStorage.removeItem('userId');
-    localStorage.removeItem('email');
-  };
-
-  // Efeito para limpar o localStorage ao montar o componente
+  // Função para limpar o localStorage ao montar o componente
   useEffect(() => {
+    const clearLocalStorage = () => {
+      localStorage.removeItem('userId');
+      localStorage.removeItem('email');
+    };
+
     clearLocalStorage();
   }, []);
 
@@ -21,7 +20,9 @@ function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
     <SessionProvider session={session}>
       <ThemeProvider>
         <MainContainer>
+          {/* Garante que o MainContainer tenha estilos e comportamentos globais */}
           <div className="overflow-hidden" />
+          {/* Renderiza o Componente da página atual */}
           <Component {...pageProps} />
         </MainContainer>
       </ThemeProvider>
@@ -30,4 +31,3 @@ function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
 }
 
 export default MyApp;
-
