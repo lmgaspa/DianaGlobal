445114a64f037de60698a344f1000cbca6d65e83
Luiz <seu-email@example.com>
2024-07-02 09:08:55 -0300

    middleware_dashboard

--- middleware.ts
+++ middleware.ts
@@ -4,31 +4,24 @@ import type { NextRequest } from 'next/server';
 
 export async function authorize(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname, origin } = req.nextUrl; // Extrai o caminho e a origem da URL da requisição
+  const { pathname, origin } = req.nextUrl;
 
-  // Permite a requisição se uma das condições abaixo for verdadeira...
-  // 1. É uma requisição para autenticação ou busca de provedor pelo next-auth
-  // 2. O token existe e é válido
   if (pathname.startsWith('/api/auth/') || token) {
-    return NextResponse.next(); // Permite que a requisição continue para o próximo middleware ou rota
+    return NextResponse.next();
   }
 
-  // Redireciona para a página de login se o usuário não tiver um token e estiver acessando uma rota protegida
   if (!token && pathname.startsWith('/protected/')) {
-    return NextResponse.redirect(`${origin}/login`); // Redireciona para a página de login com URL absoluta
+    return NextResponse.redirect(`${origin}/login`);
   }
 
-  // Para rotas que não são de API e requerem autenticação, redireciona para a página de login
   if (!token && !pathname.startsWith('/api/')) {
-    return NextResponse.redirect(`${origin}/login`); // Redireciona para a página de login com URL absoluta
+    return NextResponse.redirect(`${origin}/login`);
   }
 
-  // Se não se encaixar em nenhuma das condições acima, permite que a requisição continue
   return NextResponse.next();
 }
 
 export const config = {
-  // Declara as rotas protegidas que serão interceptadas por este middleware
   matcher: ['/protected/:path*'],
 };
 
