622c51f05e4ac0a538bb90917cdbe64cc3178fd5
Luiz <seu-email@example.com>
2024-07-02 09:05:16 -0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { getToken } from 'next-auth/jwt'; // Importa a função getToken do pacote next-auth/jwt para decodificar o token JWT
-import { NextResponse } from 'next/server'; // Importa NextResponse do pacote next/server para gerar respostas HTTP
-import type { NextRequest } from 'next/server'; // Importa o tipo NextRequest do pacote next/server para o objeto de requisição do Next.js
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export async function authorize(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname } = req.nextUrl; // Extrai o caminho da URL da requisição
+  const { pathname, origin } = req.nextUrl; // Extrai o caminho e a origem da URL da requisição
 
   // Permite a requisição se uma das condições abaixo for verdadeira...
   // 1. É uma requisição para autenticação ou busca de provedor pelo next-auth
@@ -15,12 +15,12 @@ export async function authorize(req: NextRequest) {
 
   // Redireciona para a página de login se o usuário não tiver um token e estiver acessando uma rota protegida
   if (!token && pathname.startsWith('/protected/')) {
-    return NextResponse.redirect('/login'); // Redireciona para a página de login
+    return NextResponse.redirect(`${origin}/login`); // Redireciona para a página de login com URL absoluta
   }
 
   // Para rotas que não são de API e requerem autenticação, redireciona para a página de login
   if (!token && !pathname.startsWith('/api/')) {
-    return NextResponse.redirect('/login'); // Redireciona para a página de login
+    return NextResponse.redirect(`${origin}/login`); // Redireciona para a página de login com URL absoluta
   }
 
   // Se não se encaixar em nenhuma das condições acima, permite que a requisição continue
