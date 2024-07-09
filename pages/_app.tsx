f2ebe32c17e0121510cd6a67d1d7d9e133021e51
Luiz <seu-email@example.com>
2024-07-09 20:41:49 -0300

    ErrorBoundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
+// src/pages/_app.tsx
 import { ThemeProvider } from '../context/ThemeContext';
 import MainContainer from '../components/GlobalComponent/MainComponent';
 import { AppProps } from 'next/app';
 import React, { useEffect } from 'react';
 import { SessionProvider } from "next-auth/react";
+import ErrorBoundary from '../components/ErrorBoundary';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // Função para limpar o localStorage ao montar o componente
   useEffect(() => {
     const clearLocalStorage = () => {
@@ -19,12 +21,15 @@ function MyApp({ Component, pageProps: session, ...pageProps }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider>
-        <MainContainer>
-          {/* Garante que o MainContainer tenha estilos e comportamentos globais */}
-          <div className="overflow-hidden" />
-          {/* Renderiza o Componente da página atual */}
-          <Component {...pageProps} />
-        </MainContainer>
+        <ErrorBoundary>
+          <MainContainer>
+            {/* Garante que o MainContainer tenha estilos e comportamentos globais */}
+            <div className="overflow-hidden">
+              {/* Renderiza o Componente da página atual */}
+              <Component {...pageProps} />
+            </div>
+          </MainContainer>
+        </ErrorBoundary>
       </ThemeProvider>
     </SessionProvider>
   );
