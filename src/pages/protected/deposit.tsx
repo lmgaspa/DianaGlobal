810f70dc260591638168d9895a7eec021c73119c
Luiz <seu-email@example.com>
2024-07-10 03:11:31 -0300

    fix py

--- src/pages/protected/deposit.tsx
+++ src/pages/protected/deposit.tsx
@@ -143,9 +143,11 @@ const Deposit: React.FC = () => {
                     </button>
                 </div>
             </div>
-            <div className="flex w-full justify-center items-center min-h-screen h-screen bg-white dark:bg-black text-white p-6">
-                <div className="w-full sm:w-full sm:border sm:rounded-3xl md:w-5/6 lg:w-2/4 bg-blue-300 text-black dark:bg-black dark:text-white py-8 px-8 mb-12">
-                    <div className="mb-6">
+            <div className="flex w-full justify-center  min-h-screen h-screen
+       bg-white dark:bg-black text-white p-6">
+                <div className="w-full sm:w-full sm:border sm:rounded-3xl md:w-5/6 lg:w-2/4
+         bg-blue-300 text-black dark:bg-black dark:text-white py-8 px-4 mb-12 ">
+                    <div className="mb-10">
                         <h3 className="text-lg font-semibold mb-4">Select Coin</h3>
                         <Select
                             value={coinOptions.find((option) => option.value === selectedCoin)}
