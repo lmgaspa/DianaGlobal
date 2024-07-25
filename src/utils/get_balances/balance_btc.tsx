1f76ac664e9ec310e913674ab85f99f9a6d54f99
Luiz <seu-email@example.com>
2024-07-25 19:24:19 -0300

    balance

--- src/utils/get_balances/balance_btc.tsx
+++ src/utils/get_balances/balance_btc.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const getBtcBalance = async (btcAddress: string): Promise<number | null> => {
   try {
-    const response = await axios.get(`https://blockchain.info/q/addressbalance/${btcAddress}`);
-    const balanceInSatoshis = parseFloat(response.data);
+    const response = await axios.get(`https://api.blockcypher.com/v1/btc/main/addrs/${btcAddress}/balance`);
+    const balanceInSatoshis = response.data.balance;
     return balanceInSatoshis / 100000000; // Convert satoshis to BTC
   } catch (error) {
     console.error('Error fetching BTC balance:', error);
