b90d8bd7caaa33c69c167438dee20a7d195b0eef
lmgaspa <@luhmgasparetto@gmaail.com>
2024-12-17 18:41:06 -0300

    metrics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,8 +8,7 @@ export default class MyDocument extends Document {
           {/* Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-VZVY5E6YNN
-`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-VZVY5E6YNN`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -17,7 +16,7 @@ export default class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'SEU_ID_ANALYTICS', {
+                gtag('config', 'G-VZVY5E6YNN', {
                   page_path: window.location.pathname,
                 });
               `,
