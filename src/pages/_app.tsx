33f0af6936a633ebf237686b43cf0551b41a5b62
lmgaspa <@luhmgasparetto@gmaail.com>
2024-12-17 18:46:11 -0300

    metrics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import { ThemeProvider } from '@/context/ThemeContext';
-import MainContainer from '@/components/GlobalComponent/MainComponent';
-import { AppProps } from 'next/app';
-import React, { useEffect } from 'react';
+import Script from "next/script";
+import { ThemeProvider } from "@/context/ThemeContext";
+import MainContainer from "@/components/GlobalComponent/MainComponent";
+import { AppProps } from "next/app";
+import React, { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
-import ErrorBoundary from '@/components/ErrorBoundary';
-import '../styles/globals.css';
+import ErrorBoundary from "@/components/ErrorBoundary";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   useEffect(() => {
     const clearLocalStorage = () => {
-      localStorage.removeItem('userId');
-      localStorage.removeItem('email');
+      localStorage.removeItem("userId");
+      localStorage.removeItem("email");
     };
 
     clearLocalStorage();
@@ -22,6 +23,24 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <ErrorBoundary>
           <MainContainer>
             <div className="overflow-hidden">
+              {/* Google Analytics */}
+              <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=G-VZVY5E6YNN`}
+              />
+              <Script
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-VZVY5E6YNN', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
               <Component {...pageProps} />
             </div>
           </MainContainer>
